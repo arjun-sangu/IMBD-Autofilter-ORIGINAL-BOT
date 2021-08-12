f63974c9d552826fb3a97d9eb7806119d922068f
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 12:25:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -85,7 +85,7 @@ async def filter(client, message):
                 await message.reply_photo(photo=poster, caption=f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n<a href="https://t.me/mlm_movies_update">ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -103,7 +103,7 @@ async def filter(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n<a href="https://t.me/mlm_movies_update">ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -148,7 +148,7 @@ async def group(client, message):
             if poster:
                 await message.reply_photo(photo=poster, caption=f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>Name {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n<a href="https://t.me/mlm_movies_update">ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -166,7 +166,7 @@ async def group(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Movie {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Move {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>Move {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n<a href="https://t.me/mlm_movies_update">ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
