6df48f5bfdfb23f0c89ee25d5ecb6ba963d71e9b
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 18:14:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -298,7 +298,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton("💢 NEW MOVIES 💢", url="https://t.me/MLM_ROKES")
                     ]
                     ]
                 
@@ -329,7 +329,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton("💢 NEW MOVIES 💢", url="https://t.me/MLM_ROKES")
                     ]
                     ]
                 
