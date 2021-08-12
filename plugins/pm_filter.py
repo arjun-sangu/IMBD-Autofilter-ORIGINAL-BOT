273e9bfc4957cd69ec853283873ff613d10db32b
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 16:49:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,8 +76,8 @@ async def filter(client, message):
         else:
             buttons = btn
             buttons.append(
-                 [InlineKeyboardButton(text="🎯 Pages 1/1 🎯",callback_data="pages"),
-                 [InlineKeyboardButton(text="❗HOW TO GET FILES❗", url="https://telegra.ph/HOW-TO-GET-FILES-07-19")]
+                 [InlineKeyboardButton(text="🎯 Pages 1/1 🎯",callback_data="pages")],
+                 [InlineKeyboardButton(text="❗HOW TO GET FILES❗", url="https://telegra.ph/HOW-TO-GET-FILES-07-19")
             )
             poster=None
             if API_KEY:
