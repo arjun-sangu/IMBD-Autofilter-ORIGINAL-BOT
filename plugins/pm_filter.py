b35d604d1a7a73172438fd576867f2d908559483
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 12:10:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ async def filter(client, message):
         else:
             buttons = btn
             buttons.append(
-                [InlineKeyboardButton(text="◀️ Pages 1/1 ▶️",callback_data="pages")]
+                [InlineKeyboardButton(text="🎯 Pages 1/1 🎯",callback_data="pages")]
             )
             poster=None
             if API_KEY:
