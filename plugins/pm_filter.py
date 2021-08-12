e29b228a51ed45b1dfb6f575fab50ab0136a2375
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 12:53:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -164,7 +164,7 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-(reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>Movie {search} ‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
             await message.reply_text(f"<b>Move {search} ‌‌‌‌‎ ­  ­  ­  ­  ­    </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
