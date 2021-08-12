de98568100e57244a3ab2bad3e5baaa25b5ee792
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-12 20:21:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,10 +82,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -101,9 +101,9 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­     \n🤔HOW TO GET FILES👉 /file \n\n <a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­     \n🤔HOW TO GET FILES👉 /file \n\n <a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -146,9 +146,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n🤔HOW TO GET FILES👉 /file \n\n <a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a></b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n✅  UPLOADED : @MLM_ROKES\n<a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n✅  UPLOADED : @MLM_ROKES\n<a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -164,9 +164,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‎ ­  ­  ­  ­  ­  \n✅  UPLOADED : @MLM_ROKES\n<a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST : {search} ‌‎ ­  ­  ­  ­  ­  \n✅  UPLOADED : @MLM_ROKES\n<a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­    \n✅  UPLOADED : @MLM_ROKES\n<a href='{TUTORIAL}'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🎬  YOUR REQEST : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­    \n✅  UPLOADED : @MLM_ROKES\n<a href='@mlm_movies_update'>ഈ എഴുത്തിൽ ക്ലിക്ക് ചെയ്ത് കിട്ടുന്ന ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</a> \n\n🤔HOW TO GET FILES👉 /file</b>", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
