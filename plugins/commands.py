579e926ef9850ade52969cf7601deb4ef29be937
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-14 15:01:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,7 +93,7 @@ async def start(bot, cmd):
         )
     else:
         await cmd.reply_text(
-            START_MSG,
+            text="{START_MSG}",
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
