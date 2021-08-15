b49d93576d4be9d1356590aa6c4aee9946224403
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-08-15 09:59:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,10 +82,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST :</b> {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {total} FILES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനെലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക></b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST :</b> {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {data['total']} FILES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനെലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക></b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n FOUND : {total} PAGES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n FOUND : {data['total']} PAGES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -101,9 +101,9 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {total} FILES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനെലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {data['total']} FILES<b>\n🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനെലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­     \n PAGES : {total} FILES\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­     \n PAGES : {data['total']} FILES\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -146,9 +146,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {total}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n PAGES : {data['total']}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­   \n PAGES : {total}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­   \n PAGES : {data['total']}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -164,9 +164,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQES</b>T : {search} ‌‎ ­  ­  ­  ­  ­  \n PAGES : {total}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🎬  YOUR REQES</b>T : {search} ‌‎ ­  ­  ­  ­  ­  \n PAGES : {data['total']}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­    \n PAGES : {total}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🎬  YOUR REQEST</b> : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­    \n PAGES : {data['total']}\n<b>🤔  HOW TO GET FILES👉 /file \n\n@MLM_ROKES ഈ ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം താഴെ ഉള്ള ബട്ടൺ ക്ലിക്ക് ചെയ്യുക</b>", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
